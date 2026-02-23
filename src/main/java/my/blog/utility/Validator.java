69b156e3dfb3201e84c2486d882687f3a9e21f34
Margarita <rl-c@yandex.ru>
2026-02-23 21:01:17 +0300

    add validator

--- src/main/java/my/blog/utility/Validator.java
+++ src/main/java/my/blog/utility/Validator.java
@@ -1,4 +1,53 @@
-package my.blog.validator;
+package my.blog.utility;
+
+import my.blog.dto.CommentDto;
+import my.blog.dto.CommentRequestDto;
+import my.blog.dto.PostRequestDto;
+import my.blog.dto.PostUpdateRequestDto;
 
 public class Validator {
+
+    public static void validatePostId(long postId) {
+        if (postId <= 0) {
+            throw new IllegalArgumentException("postId must be positive");
+        }
+    }
+
+    public static void validateCommentId(long commentId) {
+        if (commentId <= 0) {
+            throw new IllegalArgumentException("Invalid commentId: " + commentId);
+        }
+    }
+
+    public static void validateCommentRequest(CommentRequestDto comment) {
+        if (comment == null || comment.getText() == null || comment.getText().isBlank()) {
+            throw new IllegalArgumentException("Comment text cannot be empty");
+        }
+        validatePostId(comment.getPostId());
+    }
+
+    public static void validateCommentDto(CommentDto comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("Comment cannot be null");
+        }
+        validateCommentId(comment.getId());
+    }
+
+    public static void validatePostRequest(PostRequestDto postRequest) {
+        if (postRequest == null) {
+            throw new IllegalArgumentException("PostRequestDto must not be null");
+        }
+        if (postRequest.getTitle() == null || postRequest.getTitle().trim().isEmpty()) {
+            throw new IllegalArgumentException("Post title must not be null or empty");
+        }
+        if (postRequest.getText() == null || postRequest.getText().trim().isEmpty()) {
+            throw new IllegalArgumentException("Post text must not be null or empty");
+        }
+    }
+
+    public static void validatePostUpdate(PostUpdateRequestDto postUpdate) {
+        validatePostId(postUpdate.getId());
+        validatePostRequest(new PostRequestDto(postUpdate.getTitle(), postUpdate.getText()));
+    }
+
 }
